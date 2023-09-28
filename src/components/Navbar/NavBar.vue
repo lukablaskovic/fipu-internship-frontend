711ec992bb287583e039f6aee85d2a2e5ba970c3
Alesandro Zuzic <azuzic@student.unipu.hr>
2023-09-29 01:50:19 +0200

    Mobile navbar animation added

--- src/components/Navbar/NavBar.vue
+++ src/components/Navbar/NavBar.vue
@@ -31,10 +31,27 @@ const isMenuNavBarActive = ref(false);
                     <BaseIcon :path="isMenuNavBarActive ? mdiClose : mdiDotsVertical" size="24" />
                 </NavBarItemPlain>
             </div>
-            <div class="max-h-screen-menu overflow-y-auto lg:overflow-visible absolute w-screen top-14 left-0 bg-gray-50 shadow-lg lg:w-auto lg:flex lg:static lg:shadow-none dark:bg-slate-800"
-                :class="[isMenuNavBarActive ? 'block' : 'hidden']">
+            <div class="max-h-screen-menu overflow-y-auto lg:overflow-visible absolute w-screen top-14 -left-2 transition-size duration-300
+                bg-gray-50 shadow-lg lg:w-auto lg:flex lg:static lg:shadow-none dark:bg-slate-800"
+                :class="[isMenuNavBarActive ? 'h-64 lg:h-auto' : 'h-0 lg:h-auto']">
                 <NavBarMenuList :menu="menu" @menu-click="menuClick" />
             </div>
         </div>
     </nav>
 </template>
+<style scoped>
+::-webkit-scrollbar {
+    width: 8px;
+}
+::-webkit-scrollbar-track-piece {
+    background: #f9fafb
+}
+::-webkit-scrollbar-thumb {
+    background: #9ca3af;
+    border-radius: 100px;
+}
+::-webkit-scrollbar-thumb:hover {
+    background: #6b7280;
+    border-radius: 32px;
+}
+</style>
