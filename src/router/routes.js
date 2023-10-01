150854704460ccadeb6264514bf79db625466090
lukablaskovic <lukablaskovic2000@gmail.com>
2023-10-01 17:20:06 +0200

    Trying to deploy using GHA

--- src/router/routes.js
+++ src/router/routes.js
@@ -58,7 +58,7 @@ const routes = [
     },
     path: "/dostupni-zadaci",
     name: "dostupni-zadaci",
-    component: () => import("@/views/admin/AvailableAssignmentsView.vue"),
+    component: () => import("@/views/admin/AvailableAssignments.vue"),
   },
   {
     meta: {
