70f1a487229ce3315180594bfbb788ba2fac9462
lukablaskovic <lukablaskovic2000@gmail.com>
2023-10-08 19:25:17 +0200

    Update CompaniesView.vue

--- src/views/common/CompaniesView.vue
+++ src/views/common/CompaniesView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { mainStore, adminStore } from "@/main.js";
 import { mdiDomain, mdiDomainPlus, mdiLaptop } from "@mdi/js";
 import SectionMain from "@/components/Section/SectionMain.vue";
@@ -13,9 +13,14 @@ import TableNewCompanies from "@/components/Tables/TableNewCompanies.vue";
 import { router } from "@/router";
 import BaseIcon from "@/components/Base/BaseIcon.vue";
 import CardBoxComponentEmpty from "@/components/Cardbox/CardBoxComponentEmpty.vue";
+import FormField from "@/components/Form/FormField.vue";
+import FormControl from "@/components/Form/FormControl.vue";
 
 const userAuthenticated = computed(() => mainStore.userAuthenticated);
 const userAdmin = computed(() => mainStore.userAdmin);
+const VITE_DASHBOARD_URL = import.meta.env.VITE_DASHBOARD_URL;
+
+const addNewAssignmentLink = ref(`${VITE_DASHBOARD_URL}/#/poslodavci/novi-zadatak`);
 </script>
 
 <template>
@@ -37,10 +42,11 @@ const userAdmin = computed(() => mainStore.userAdmin);
 				<SectionTitleLineWithButton class="mt-4" :icon="mdiDomain" title="Svi Poslodavci" main> </SectionTitleLineWithButton>
 				<p class="mb-4">U tablici se nalaze poduzeća koja su registrirana za provođenje studentske prakse u suradnji s Fakultetom informatike u Puli.</p>
 				<div v-if="!userAdmin" class="mb-4">
-					<p class="text-sm md:text-base">
-						Ukoliko ne možete naći zadatak koji vam se sviđa, javite se poslodavcu, predstavite se i zamolite ih da ispune zadatak
-						<a href="">putem ove poveznice</a>.
-					</p>
+					<p class="text-sm md:text-base mb-2">Ukoliko ne možete naći zadatak koji vam se sviđa, javite se poslodavcu, predstavite se i zamolite ih da ispune zadatak putem poveznice ispod.</p>
+					<FormField class="md:w-1/2" label="Forma za prijavu novog zadatka">
+						<FormControl v-model="addNewAssignmentLink" :icon-left="mdiClipboardCheck" :icon-right="mdiContentCopy" name="addNewAssignmentLink" readonly copyable />
+					</FormField>
+
 					<p class="flex flex-wrap items-center text-sm md:text-base">
 						Nakon što poslodavac ispuni zadatak, prijavite ga u
 						<span class="text-fipu_blue cursor-pointer inline-flex items-center md:ml-1 whitespace-normal" @click="router.push('/moja-praksa')">
