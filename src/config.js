4a5c5391959b6d70204a2eaa17d7fb316c736b17
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2024-09-12 13:35:37 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,9 +6,9 @@ export const containerMaxW = "xl:max-w-7xl xl:mx-auto";
 
 export const endpoints = {
 	VITE_FRONTEND_URL: "https://fipu-praksa.unipu.hr",
-	VITE_GATEWAY_API_URL: "http://fipubot.unipu.hr:8081",
-	VITE_BPMN_ENGINE_API_URL: "http://fipubot.unipu.hr:8080",
-	VITE_BASEROW_API_URL: "http://fipubot.unipu.hr:8082",
-	VITE_SENDGRID_API_URL: "http://fipubot.unipu.hr:8083",
-	VITE_PDF_API_URL: "http://fipubot.unipu.hr:8084",
+	VITE_GATEWAY_API_URL: "https://fipu-praksa.unipu.hr/api/gateway-api",
+	VITE_BPMN_ENGINE_API_URL: "https://fipu-praksa.unipu.hr/api/python-bpmn-engine",
+	VITE_BASEROW_API_URL: "https://fipu-praksa.unipu.hr/api/baserow-connector",
+	VITE_SENDGRID_API_URL: "https://fipu-praksa.unipu.hr/api/sendgrid-connector",
+	VITE_PDF_API_URL: "https://fipu-praksa.unipu.hr/api/pdf-connector",
 };
